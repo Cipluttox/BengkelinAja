924fbc8bd7309ce55b38ef4724bbf5c95a29c71b
Irvan Akbar <irvanakbar20000@gmail.com>
2024-12-17 10:54:58 +0700

    Update KelolaDataBengkel(Edit).Designer.cs

--- View/KelolaDataBengkel(Edit).Designer.cs
+++ View/KelolaDataBengkel(Edit).Designer.cs
@@ -1,6 +1,6 @@
 namespace BengkelinAja___Final_Project.View
 {
-    partial class HomePagePelanggan
+    partial class KelolaDataBengkel_Edit_
     {
         /// <summary>
         /// Required designer variable.
@@ -28,88 +28,300 @@ namespace BengkelinAja___Final_Project.View
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HomePagePelanggan));
-            bt_PesanBengkel = new Button();
-            bt_Riwayat = new Button();
-            bt_Logout = new Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(KelolaDataBengkel_Edit_));
+            txtJamTutup = new TextBox();
+            txtJamBuka = new TextBox();
+            label5 = new Label();
+            label4 = new Label();
+            txtAlamat = new TextBox();
+            label3 = new Label();
+            cbServisDarurat = new CheckBox();
+            cbServisGaransi = new CheckBox();
+            cbGantiOli = new CheckBox();
+            cbServisRutin = new CheckBox();
+            label2 = new Label();
+            txtNamaBengkel = new TextBox();
+            label1 = new Label();
+            bt_kembali = new Button();
+            bt_simpan = new Button();
+            cbSepedaMotor = new CheckBox();
+            cbKendaraanBesar = new CheckBox();
+            cbMobil = new CheckBox();
             button1 = new Button();
+            button2 = new Button();
             SuspendLayout();
             // 
-            // bt_PesanBengkel
+            // txtJamTutup
             // 
-            bt_PesanBengkel.BackColor = Color.Transparent;
-            bt_PesanBengkel.BackgroundImage = (Image)resources.GetObject("bt_PesanBengkel.BackgroundImage");
-            bt_PesanBengkel.FlatAppearance.BorderSize = 0;
-            bt_PesanBengkel.FlatStyle = FlatStyle.Flat;
-            bt_PesanBengkel.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            bt_PesanBengkel.ForeColor = Color.Transparent;
-            bt_PesanBengkel.Location = new Point(236, 169);
-            bt_PesanBengkel.Name = "bt_PesanBengkel";
-            bt_PesanBengkel.Size = new Size(333, 60);
-            bt_PesanBengkel.TabIndex = 2;
-            bt_PesanBengkel.UseVisualStyleBackColor = false;
-            bt_PesanBengkel.Click += bt_PesanBengkel_Click;
+            txtJamTutup.BackColor = Color.White;
+            txtJamTutup.Location = new Point(362, 295);
+            txtJamTutup.Name = "txtJamTutup";
+            txtJamTutup.Size = new Size(244, 27);
+            txtJamTutup.TabIndex = 31;
             // 
-            // bt_Riwayat
+            // txtJamBuka
             // 
-            bt_Riwayat.BackColor = Color.Transparent;
-            bt_Riwayat.BackgroundImage = (Image)resources.GetObject("bt_Riwayat.BackgroundImage");
-            bt_Riwayat.FlatAppearance.BorderSize = 0;
-            bt_Riwayat.FlatStyle = FlatStyle.Flat;
-            bt_Riwayat.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            bt_Riwayat.ForeColor = Color.Transparent;
-            bt_Riwayat.Location = new Point(236, 256);
-            bt_Riwayat.Name = "bt_Riwayat";
-            bt_Riwayat.Size = new Size(329, 52);
-            bt_Riwayat.TabIndex = 3;
-            bt_Riwayat.UseVisualStyleBackColor = false;
-            bt_Riwayat.Click += bt_Riwayat_Click;
+            txtJamBuka.BackColor = Color.White;
+            txtJamBuka.Location = new Point(362, 252);
+            txtJamBuka.Name = "txtJamBuka";
+            txtJamBuka.Size = new Size(244, 27);
+            txtJamBuka.TabIndex = 30;
             // 
-            // bt_Logout
+            // label5
             // 
-            bt_Logout.BackColor = Color.Transparent;
-            bt_Logout.FlatAppearance.BorderSize = 0;
-            bt_Logout.FlatStyle = FlatStyle.Flat;
-            bt_Logout.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            bt_Logout.ForeColor = Color.Transparent;
-            bt_Logout.Location = new Point(696, -1);
-            bt_Logout.Name = "bt_Logout";
-            bt_Logout.Size = new Size(92, 39);
-            bt_Logout.TabIndex = 6;
-            bt_Logout.UseVisualStyleBackColor = false;
-            bt_Logout.Click += bt_Logout_Click;
+            label5.AutoSize = true;
+            label5.BackColor = SystemColors.ButtonHighlight;
+            label5.Location = new Point(139, 369);
+            label5.Name = "label5";
+            label5.Size = new Size(0, 20);
+            label5.TabIndex = 29;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = SystemColors.ButtonHighlight;
+            label4.Location = new Point(139, 329);
+            label4.Name = "label4";
+            label4.Size = new Size(0, 20);
+            label4.TabIndex = 28;
+            // 
+            // txtAlamat
+            // 
+            txtAlamat.BackColor = Color.White;
+            txtAlamat.Location = new Point(361, 211);
+            txtAlamat.Name = "txtAlamat";
+            txtAlamat.Size = new Size(244, 27);
+            txtAlamat.TabIndex = 27;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = SystemColors.ButtonHighlight;
+            label3.Location = new Point(139, 283);
+            label3.Name = "label3";
+            label3.Size = new Size(0, 20);
+            label3.TabIndex = 26;
+            // 
+            // cbServisDarurat
+            // 
+            cbServisDarurat.AutoSize = true;
+            cbServisDarurat.BackColor = Color.FromArgb(250, 206, 176);
+            cbServisDarurat.Font = new Font("Segoe UI", 7F);
+            cbServisDarurat.Location = new Point(484, 162);
+            cbServisDarurat.Name = "cbServisDarurat";
+            cbServisDarurat.Size = new Size(101, 19);
+            cbServisDarurat.TabIndex = 25;
+            cbServisDarurat.Text = "Servis Darurat";
+            cbServisDarurat.UseVisualStyleBackColor = false;
+            // 
+            // cbServisGaransi
+            // 
+            cbServisGaransi.AutoSize = true;
+            cbServisGaransi.BackColor = Color.FromArgb(250, 206, 176);
+            cbServisGaransi.Font = new Font("Segoe UI", 7F);
+            cbServisGaransi.Location = new Point(484, 137);
+            cbServisGaransi.Name = "cbServisGaransi";
+            cbServisGaransi.Size = new Size(101, 19);
+            cbServisGaransi.TabIndex = 24;
+            cbServisGaransi.Text = "Servis Garansi";
+            cbServisGaransi.UseVisualStyleBackColor = false;
+            // 
+            // cbGantiOli
+            // 
+            cbGantiOli.AutoSize = true;
+            cbGantiOli.BackColor = Color.FromArgb(250, 206, 176);
+            cbGantiOli.Font = new Font("Segoe UI", 7F);
+            cbGantiOli.Location = new Point(362, 162);
+            cbGantiOli.Name = "cbGantiOli";
+            cbGantiOli.Size = new Size(75, 19);
+            cbGantiOli.TabIndex = 23;
+            cbGantiOli.Text = "Ganti Oli";
+            cbGantiOli.UseVisualStyleBackColor = false;
+            // 
+            // cbServisRutin
+            // 
+            cbServisRutin.AutoSize = true;
+            cbServisRutin.BackColor = Color.FromArgb(250, 206, 176);
+            cbServisRutin.Font = new Font("Segoe UI", 7F);
+            cbServisRutin.Location = new Point(362, 137);
+            cbServisRutin.Name = "cbServisRutin";
+            cbServisRutin.Size = new Size(90, 19);
+            cbServisRutin.TabIndex = 22;
+            cbServisRutin.Text = "Servis Rutin";
+            cbServisRutin.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = SystemColors.ButtonHighlight;
+            label2.Location = new Point(138, 200);
+            label2.Name = "label2";
+            label2.Size = new Size(0, 20);
+            label2.TabIndex = 21;
+            // 
+            // txtNamaBengkel
+            // 
+            txtNamaBengkel.BackColor = Color.White;
+            txtNamaBengkel.Location = new Point(362, 94);
+            txtNamaBengkel.Name = "txtNamaBengkel";
+            txtNamaBengkel.Size = new Size(244, 27);
+            txtNamaBengkel.TabIndex = 20;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.ButtonHighlight;
+            label1.Location = new Point(138, 150);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 20);
+            label1.TabIndex = 19;
+            // 
+            // bt_kembali
+            // 
+            bt_kembali.BackColor = Color.Transparent;
+            bt_kembali.BackgroundImage = (Image)resources.GetObject("bt_kembali.BackgroundImage");
+            bt_kembali.FlatAppearance.BorderSize = 0;
+            bt_kembali.FlatStyle = FlatStyle.Flat;
+            bt_kembali.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            bt_kembali.ForeColor = Color.Transparent;
+            bt_kembali.Location = new Point(445, 388);
+            bt_kembali.Name = "bt_kembali";
+            bt_kembali.Size = new Size(74, 22);
+            bt_kembali.TabIndex = 18;
+            bt_kembali.UseVisualStyleBackColor = false;
+            bt_kembali.Click += bt_kembali_Click;
+            // 
+            // bt_simpan
+            // 
+            bt_simpan.BackColor = Color.Transparent;
+            bt_simpan.BackgroundImage = (Image)resources.GetObject("bt_simpan.BackgroundImage");
+            bt_simpan.FlatAppearance.BorderSize = 0;
+            bt_simpan.FlatStyle = FlatStyle.Flat;
+            bt_simpan.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            bt_simpan.ForeColor = Color.Transparent;
+            bt_simpan.Location = new Point(528, 386);
+            bt_simpan.Name = "bt_simpan";
+            bt_simpan.Size = new Size(74, 23);
+            bt_simpan.TabIndex = 17;
+            bt_simpan.UseVisualStyleBackColor = false;
+            bt_simpan.Click += bt_simpan_Click;
+            // 
+            // cbSepedaMotor
+            // 
+            cbSepedaMotor.AutoSize = true;
+            cbSepedaMotor.BackColor = Color.FromArgb(250, 206, 176);
+            cbSepedaMotor.Font = new Font("Segoe UI", 7F);
+            cbSepedaMotor.Location = new Point(361, 351);
+            cbSepedaMotor.Name = "cbSepedaMotor";
+            cbSepedaMotor.Size = new Size(106, 19);
+            cbSepedaMotor.TabIndex = 52;
+            cbSepedaMotor.Text = "Sepeda Motor ";
+            cbSepedaMotor.UseVisualStyleBackColor = false;
+            // 
+            // cbKendaraanBesar
+            // 
+            cbKendaraanBesar.AutoSize = true;
+            cbKendaraanBesar.BackColor = Color.FromArgb(250, 206, 176);
+            cbKendaraanBesar.Font = new Font("Segoe UI", 7F);
+            cbKendaraanBesar.Location = new Point(426, 329);
+            cbKendaraanBesar.Name = "cbKendaraanBesar";
+            cbKendaraanBesar.Size = new Size(193, 19);
+            cbKendaraanBesar.TabIndex = 51;
+            cbKendaraanBesar.Text = "Kendaraan besar (Truk, Bus, dll)";
+            cbKendaraanBesar.UseVisualStyleBackColor = false;
+            // 
+            // cbMobil
+            // 
+            cbMobil.AutoSize = true;
+            cbMobil.BackColor = Color.FromArgb(250, 206, 176);
+            cbMobil.Font = new Font("Segoe UI", 7F);
+            cbMobil.Location = new Point(361, 329);
+            cbMobil.Name = "cbMobil";
+            cbMobil.Size = new Size(60, 19);
+            cbMobil.TabIndex = 50;
+            cbMobil.Text = "Mobil";
+            cbMobil.UseVisualStyleBackColor = false;
             // 
             // button1
             // 
+            button1.BackColor = Color.Transparent;
             button1.BackgroundImage = (Image)resources.GetObject("button1.BackgroundImage");
             button1.FlatAppearance.BorderSize = 0;
             button1.FlatStyle = FlatStyle.Flat;
-            button1.Location = new Point(696, 10);
+            button1.Location = new Point(20, 75);
             button1.Name = "button1";
-            button1.Size = new Size(94, 20);
-            button1.TabIndex = 7;
-            button1.UseVisualStyleBackColor = true;
+            button1.Size = new Size(150, 24);
+            button1.TabIndex = 53;
+            button1.UseVisualStyleBackColor = false;
             button1.Click += button1_Click;
             // 
-            // HomePagePelanggan
+            // button2
+            // 
+            button2.BackColor = Color.Transparent;
+            button2.BackgroundImage = (Image)resources.GetObject("button2.BackgroundImage");
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Location = new Point(21, 108);
+            button2.Name = "button2";
+            button2.Size = new Size(149, 29);
+            button2.TabIndex = 54;
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // KelolaDataBengkel_Edit_
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
             ClientSize = new Size(800, 450);
+            Controls.Add(button2);
             Controls.Add(button1);
-            Controls.Add(bt_Logout);
-            Controls.Add(bt_Riwayat);
-            Controls.Add(bt_PesanBengkel);
-            Name = "HomePagePelanggan";
-            Text = "HomePagePelanggan";
+            Controls.Add(cbSepedaMotor);
+            Controls.Add(cbKendaraanBesar);
+            Controls.Add(cbMobil);
+            Controls.Add(txtJamTutup);
+            Controls.Add(txtJamBuka);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(txtAlamat);
+            Controls.Add(label3);
+            Controls.Add(cbServisDarurat);
+            Controls.Add(cbServisGaransi);
+            Controls.Add(cbGantiOli);
+            Controls.Add(cbServisRutin);
+            Controls.Add(label2);
+            Controls.Add(txtNamaBengkel);
+            Controls.Add(label1);
+            Controls.Add(bt_kembali);
+            Controls.Add(bt_simpan);
+            Name = "KelolaDataBengkel_Edit_";
+            Text = "KelolaDataBengkel_Edit_";
+            Load += KelolaDataBengkel_Edit__Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
-        private Button bt_PesanBengkel;
-        private Button bt_Riwayat;
-        private Button bt_Logout;
+
+        private TextBox txtJamTutup;
+        private TextBox txtJamBuka;
+        private Label label5;
+        private Label label4;
+        private TextBox txtAlamat;
+        private Label label3;
+        private CheckBox cbServisDarurat;
+        private CheckBox cbServisGaransi;
+        private CheckBox cbGantiOli;
+        private CheckBox cbServisRutin;
+        private Label label2;
+        private TextBox txtNamaBengkel;
+        private Label label1;
+        private Button bt_kembali;
+        private Button bt_simpan;
+        private CheckBox cbSepedaMotor;
+        private CheckBox cbKendaraanBesar;
+        private CheckBox cbMobil;
         private Button button1;
+        private Button button2;
     }
 }
