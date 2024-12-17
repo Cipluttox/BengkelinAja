e94ad88c9f2587a8992139b625852e720db13841
Tantri Okta Puspita Sari <oktatan12@gmail.com>
2024-12-17 10:18:48 +0700

    Update LihatJadwalServis.Designer.cs

--- View/LihatJadwalServis.Designer.cs
+++ View/LihatJadwalServis.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace BengkelinAja.View
+namespace BengkelinAja___Final_Project.View
 {
     partial class LihatJadwalServis
     {
@@ -28,63 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LihatJadwalServis));
             pictureBox1 = new PictureBox();
-            dataGridView1 = new DataGridView();
-            btn_kembali = new Button();
+            dataGridViewServis = new DataGridView();
+            button1 = new Button();
+            bt_kembali = new Button();
+            button2 = new Button();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewServis).BeginInit();
             SuspendLayout();
             // 
             // pictureBox1
             // 
-            pictureBox1.Image = Properties.Resources.Daftar_jadwal_servis__1_;
-            pictureBox1.Location = new Point(-2, -3);
+            pictureBox1.BackgroundImage = (Image)resources.GetObject("pictureBox1.BackgroundImage");
+            pictureBox1.Location = new Point(-2, -1);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(804, 458);
+            pictureBox1.Size = new Size(804, 451);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
             // 
-            // dataGridView1
+            // dataGridViewServis
             // 
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(70, 146);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.RowHeadersWidth = 51;
-            dataGridView1.Size = new Size(651, 253);
-            dataGridView1.TabIndex = 1;
+            dataGridViewServis.AllowUserToAddRows = false;
+            dataGridViewServis.AllowUserToDeleteRows = false;
+            dataGridViewServis.BackgroundColor = Color.Silver;
+            dataGridViewServis.BorderStyle = BorderStyle.None;
+            dataGridViewServis.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewServis.Location = new Point(235, 82);
+            dataGridViewServis.Name = "dataGridViewServis";
+            dataGridViewServis.RowHeadersWidth = 51;
+            dataGridViewServis.Size = new Size(508, 270);
+            dataGridViewServis.TabIndex = 1;
+            dataGridViewServis.CellContentClick += dataGridViewServis_CellContentClick;
             // 
-            // btn_kembali
+            // button1
             // 
-            btn_kembali.BackColor = Color.Blue;
-            btn_kembali.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btn_kembali.ForeColor = SystemColors.HighlightText;
-            btn_kembali.Location = new Point(636, 41);
-            btn_kembali.Name = "btn_kembali";
-            btn_kembali.Size = new Size(123, 42);
-            btn_kembali.TabIndex = 2;
-            btn_kembali.Text = "Kembali";
-            btn_kembali.UseVisualStyleBackColor = false;
-            btn_kembali.Click += btn_kembali_Click;
+            button1.BackColor = Color.Transparent;
+            button1.BackgroundImage = (Image)resources.GetObject("button1.BackgroundImage");
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Location = new Point(19, 114);
+            button1.Name = "button1";
+            button1.Size = new Size(147, 29);
+            button1.TabIndex = 2;
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += bt_kembali_Click;
+            // 
+            // bt_kembali
+            // 
+            bt_kembali.BackgroundImage = (Image)resources.GetObject("bt_kembali.BackgroundImage");
+            bt_kembali.FlatAppearance.BorderSize = 0;
+            bt_kembali.FlatStyle = FlatStyle.Flat;
+            bt_kembali.Location = new Point(678, 368);
+            bt_kembali.Name = "bt_kembali";
+            bt_kembali.Size = new Size(64, 24);
+            bt_kembali.TabIndex = 3;
+            bt_kembali.UseVisualStyleBackColor = true;
+            bt_kembali.Click += bt_kembali_Click_1;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Transparent;
+            button2.BackgroundImage = (Image)resources.GetObject("button2.BackgroundImage");
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Location = new Point(12, 36);
+            button2.Name = "button2";
+            button2.Size = new Size(154, 29);
+            button2.TabIndex = 4;
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
             // 
             // LihatJadwalServis
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(btn_kembali);
-            Controls.Add(dataGridView1);
+            Controls.Add(button2);
+            Controls.Add(bt_kembali);
+            Controls.Add(button1);
+            Controls.Add(dataGridViewServis);
             Controls.Add(pictureBox1);
             Name = "LihatJadwalServis";
             Text = "LihatJadwalServis";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewServis).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
         private PictureBox pictureBox1;
-        private DataGridView dataGridView1;
-        private Button btn_kembali;
+        private DataGridView dataGridViewServis;
+        private Button button1;
+        private Button bt_kembali;
+        private Button button2;
     }
 }
