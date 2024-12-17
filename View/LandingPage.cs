4946d690ae93c7499ce7e321bd413d414d54b330
Chintya13 <chinchintya001@gmail.com>
2024-12-17 10:12:00 +0700

    Update LandingPage.cs

--- View/LandingPage.cs
+++ View/LandingPage.cs
@@ -1,59 +1,61 @@
-using BengkelinAja.Context;
-using BengkelinAja.View;
+namespace BengkelinAja___Final_Project;
+using BengkelinAja___Final_Project.Context;
+using BengkelinAja___Final_Project.View;
+using Microsoft.VisualBasic.Logging;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
-namespace BengkelinAja
+public partial class Form1 : Form
 {
-    public partial class Form1 : Form
+    public Form1()
     {
-        public Form1()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+        this.StartPosition = FormStartPosition.CenterScreen;
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-        }
+    }
 
-        private void bt_Login_Click(object sender, EventArgs e)
-        {
-            this.Hide();
-            Login HalLogin = new Login();
-            if (HalLogin.ShowDialog() == DialogResult.OK)
-            {
-                //this.Close();
-            }
-            else
-            {
-                this.Show();
-            }
+    private void Form1_Load(object sender, EventArgs e)
+    {
+    }
 
+    private void bt_Login_Click(object sender, EventArgs e)
+    {
+        this.Hide();
+        Login HalLogin = new Login();
+        if (HalLogin.ShowDialog() == DialogResult.OK)
+        {
+            //this.Close();
         }
-        private void bt_RegistPelanggan_Click(object sender, EventArgs e)
+        else
         {
             this.Show();
-            Register_Pelanggan halPelanggan = new Register_Pelanggan();
-            if (halPelanggan.ShowDialog() == DialogResult.OK) { 
-                //this.Close(); 
-            }
-            else { this.Show(); }
         }
 
-        private void bt_RegistBengkel_Click(object sender, EventArgs e)
+    }
+    private void bt_RegistPelanggan_Click(object sender, EventArgs e)
+    {
+        this.Hide();
+        RegisterPengelolaBengkel HalRegistBengkel = new RegisterPengelolaBengkel();
+        if (HalRegistBengkel.ShowDialog() == DialogResult.OK)
         {
-            this.Hide();
-            RegisterPengelolaBengkel HalRegistBengkel = new RegisterPengelolaBengkel();
-            if (HalRegistBengkel.ShowDialog() == DialogResult.OK)
-            {
-                //this.Close(); // Tutup hanya jika pendaftaran berhasil
-            }
-            else
-            {
-                this.Show(); // Kembali ke landing page jika pendaftaran gagal/dibatalkan
-            }
+            //this.Hide() ;
         }
+        else
+        {
+            this.Show(); // Kembali ke landing page jika pendaftaran gagal/dibatalkan
+        }
+    }
 
-    } 
+    private void bt_RegistBengkel_Click(object sender, EventArgs e)
+    {
+        this.Hide();
+        Register_Pelanggan halPelanggan = new Register_Pelanggan();
+        if (halPelanggan.ShowDialog() == DialogResult.OK)
+        {
+            //this.Hide(); 
+        }
+        else { this.Show(); }
+    }
+
 }
- 
+
 
