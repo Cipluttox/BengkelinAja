c5676e0f7016cfbdd079bf9341fcf712a9d84c2e
Chintya13 <chinchintya001@gmail.com>
2024-12-17 10:21:20 +0700

    Update Login.Designer.cs

--- View/Login.Designer.cs
+++ View/Login.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace BengkelinAja.View
+namespace BengkelinAja___Final_Project.View
 {
     partial class Login
     {
@@ -31,22 +31,24 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
             pictureBox1 = new PictureBox();
             label1 = new Label();
-            tbUsername = new TextBox();
             label2 = new Label();
-            tbPassword = new TextBox();
+            pictureBox2 = new PictureBox();
             btLogin = new Button();
+            tbUsername = new TextBox();
+            tbPassword = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
             // 
             // pictureBox1
             // 
-            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(-2, 3);
+            pictureBox1.Location = new Point(0, 0);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(800, 450);
-            pictureBox1.TabIndex = 2;
+            pictureBox1.Size = new Size(100, 50);
+            pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
-            pictureBox1.Click += pictureBox1_Click;
             // 
             // label1
             // 
@@ -58,14 +60,6 @@
             label1.TabIndex = 3;
             label1.Text = "Username : ";
             // 
-            // tbUsername
-            // 
-            tbUsername.Location = new Point(564, 194);
-            tbUsername.Name = "tbUsername";
-            tbUsername.Size = new Size(159, 27);
-            tbUsername.TabIndex = 4;
-            tbUsername.TextChanged += tbUsername_TextChanged;
-            // 
             // label2
             // 
             label2.AutoSize = true;
@@ -76,40 +70,79 @@
             label2.TabIndex = 5;
             label2.Text = "Password : ";
             // 
-            // tbPassword
+            // pictureBox2
             // 
-            tbPassword.Location = new Point(564, 243);
-            tbPassword.Name = "tbPassword";
-            tbPassword.Size = new Size(159, 27);
-            tbPassword.TabIndex = 6;
-            tbPassword.TextChanged += tbPassword_TextChanged;
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(1, -1);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(800, 452);
+            pictureBox2.TabIndex = 0;
+            pictureBox2.TabStop = false;
             // 
             // btLogin
             // 
-            btLogin.BackColor = Color.DarkOrange;
-            btLogin.ForeColor = Color.White;
-            btLogin.Location = new Point(553, 314);
+            btLogin.BackColor = Color.Transparent;
+            btLogin.BackgroundImage = (Image)resources.GetObject("btLogin.BackgroundImage");
+            btLogin.FlatAppearance.BorderSize = 0;
+            btLogin.FlatStyle = FlatStyle.Flat;
+            btLogin.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btLogin.ForeColor = Color.Transparent;
+            btLogin.Location = new Point(652, 308);
             btLogin.Name = "btLogin";
-            btLogin.Size = new Size(94, 29);
+            btLogin.Size = new Size(83, 29);
             btLogin.TabIndex = 7;
-            btLogin.Text = "Login";
             btLogin.UseVisualStyleBackColor = false;
             btLogin.Click += btLogin_Click;
             // 
+            // tbUsername
+            // 
+            tbUsername.BackColor = Color.WhiteSmoke;
+            tbUsername.BorderStyle = BorderStyle.None;
+            tbUsername.ForeColor = Color.Black;
+            tbUsername.Location = new Point(478, 188);
+            tbUsername.Name = "tbUsername";
+            tbUsername.Size = new Size(248, 20);
+            tbUsername.TabIndex = 6;
+            // 
+            // tbPassword
+            // 
+            tbPassword.BackColor = Color.WhiteSmoke;
+            tbPassword.BorderStyle = BorderStyle.None;
+            tbPassword.Location = new Point(478, 253);
+            tbPassword.Name = "tbPassword";
+            tbPassword.Size = new Size(237, 20);
+            tbPassword.TabIndex = 7;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.Location = new Point(472, 195);
+            label3.Name = "label3";
+            label3.Size = new Size(0, 20);
+            label3.TabIndex = 3;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.Location = new Point(472, 242);
+            label4.Name = "label4";
+            label4.Size = new Size(0, 20);
+            label4.TabIndex = 8;
+            // 
             // Login
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
-            AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(btLogin);
+            Controls.Add(label4);
+            Controls.Add(label3);
             Controls.Add(tbPassword);
-            Controls.Add(label2);
             Controls.Add(tbUsername);
-            Controls.Add(label1);
-            Controls.Add(pictureBox1);
+            Controls.Add(btLogin);
+            Controls.Add(pictureBox2);
             Name = "Login";
-            Text = "Login";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -118,9 +151,13 @@
 
         private PictureBox pictureBox1;
         private Label label1;
-        private TextBox tbUsername;
         private Label label2;
-        private TextBox tbPassword;
+        private PictureBox pictureBox2;
         private Button btLogin;
+        private TextBox tbUsername;
+        private TextBox tbPassword;
+        private Label label3;
+        private Label label4;
     }
 }
+
