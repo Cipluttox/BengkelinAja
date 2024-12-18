e80d74c994fdbd52caa67a7cf06c06edc69f4a5e
Nabila Laila <148866931+nabilalaila@users.noreply.github.com>
2024-12-18 21:55:36 +0700

    Update RegistrasiPengelolaBengkel.Designer.cs

--- View/RegistrasiPengelolaBengkel.Designer.cs
+++ View/RegistrasiPengelolaBengkel.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace BengkelinAja.View
+namespace BengkelinAja___Final_Project.View
 {
-    partial class Registrasi_Pengelola_Bengke_
+    partial class RegisterPengelolaBengkel
     {
         /// <summary>
         /// Required designer variable.
@@ -28,31 +28,267 @@
         /// </summary>
         private void InitializeComponent()
         {
-            label1 = new Label();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RegisterPengelolaBengkel));
+            L2_GantiOli = new CheckBox();
+            L1_ServisRutin = new CheckBox();
+            L5_ServisDarurat = new CheckBox();
+            PAdd = new Button();
+            L3_ServisGaransi = new CheckBox();
+            AP_Pengelola = new TextBox();
+            E_Pengelola = new TextBox();
+            NT_Pengelola = new TextBox();
+            NP_Pengelola = new TextBox();
+            PW_Pengelola = new TextBox();
+            US_Pengelola = new TextBox();
+            JT_Pengelola = new TextBox();
+            JB_Pengelola = new TextBox();
+            AB_Pengelola = new TextBox();
+            NB_Pengelola = new TextBox();
+            cb_SepedaMotor = new CheckBox();
+            cb_mobil = new CheckBox();
+            cb_KendaraanBesar = new CheckBox();
+            pictureBox1 = new PictureBox();
+            bt_kembali = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
-            // label1
+            // L2_GantiOli
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(435, 143);
-            label1.Name = "label1";
-            label1.Size = new Size(0, 20);
-            label1.TabIndex = 0;
+            L2_GantiOli.AutoSize = true;
+            L2_GantiOli.BackColor = Color.White;
+            L2_GantiOli.Location = new Point(479, 157);
+            L2_GantiOli.Name = "L2_GantiOli";
+            L2_GantiOli.Size = new Size(89, 24);
+            L2_GantiOli.TabIndex = 53;
+            L2_GantiOli.Text = "Ganti Oli";
+            L2_GantiOli.UseVisualStyleBackColor = false;
             // 
-            // Registrasi_Pengelola_Bengke_
+            // L1_ServisRutin
+            // 
+            L1_ServisRutin.AutoSize = true;
+            L1_ServisRutin.BackColor = Color.White;
+            L1_ServisRutin.Location = new Point(479, 129);
+            L1_ServisRutin.Name = "L1_ServisRutin";
+            L1_ServisRutin.Size = new Size(107, 24);
+            L1_ServisRutin.TabIndex = 52;
+            L1_ServisRutin.Text = "Servis Rutin";
+            L1_ServisRutin.UseVisualStyleBackColor = false;
+            // 
+            // L5_ServisDarurat
+            // 
+            L5_ServisDarurat.AutoSize = true;
+            L5_ServisDarurat.BackColor = Color.White;
+            L5_ServisDarurat.Location = new Point(592, 128);
+            L5_ServisDarurat.Name = "L5_ServisDarurat";
+            L5_ServisDarurat.Size = new Size(123, 24);
+            L5_ServisDarurat.TabIndex = 55;
+            L5_ServisDarurat.Text = "Servis Darurat";
+            L5_ServisDarurat.UseVisualStyleBackColor = false;
+            // 
+            // PAdd
+            // 
+            PAdd.BackColor = Color.Transparent;
+            PAdd.BackgroundImage = (Image)resources.GetObject("PAdd.BackgroundImage");
+            PAdd.FlatAppearance.BorderSize = 0;
+            PAdd.FlatStyle = FlatStyle.Flat;
+            PAdd.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            PAdd.ForeColor = Color.White;
+            PAdd.Location = new Point(623, 725);
+            PAdd.Name = "PAdd";
+            PAdd.Size = new Size(94, 28);
+            PAdd.TabIndex = 51;
+            PAdd.UseVisualStyleBackColor = false;
+            PAdd.Click += PAdd_Click;
+            // 
+            // L3_ServisGaransi
+            // 
+            L3_ServisGaransi.AutoSize = true;
+            L3_ServisGaransi.BackColor = Color.White;
+            L3_ServisGaransi.Location = new Point(592, 157);
+            L3_ServisGaransi.Name = "L3_ServisGaransi";
+            L3_ServisGaransi.Size = new Size(122, 24);
+            L3_ServisGaransi.TabIndex = 54;
+            L3_ServisGaransi.Text = "Servis Garansi";
+            L3_ServisGaransi.UseVisualStyleBackColor = false;
+            // 
+            // AP_Pengelola
+            // 
+            AP_Pengelola.Location = new Point(479, 596);
+            AP_Pengelola.Name = "AP_Pengelola";
+            AP_Pengelola.Size = new Size(236, 27);
+            AP_Pengelola.TabIndex = 50;
+            // 
+            // E_Pengelola
+            // 
+            E_Pengelola.Location = new Point(479, 544);
+            E_Pengelola.Name = "E_Pengelola";
+            E_Pengelola.Size = new Size(236, 27);
+            E_Pengelola.TabIndex = 48;
+            // 
+            // NT_Pengelola
+            // 
+            NT_Pengelola.Location = new Point(479, 498);
+            NT_Pengelola.Name = "NT_Pengelola";
+            NT_Pengelola.Size = new Size(236, 27);
+            NT_Pengelola.TabIndex = 47;
+            // 
+            // NP_Pengelola
+            // 
+            NP_Pengelola.Location = new Point(479, 447);
+            NP_Pengelola.Name = "NP_Pengelola";
+            NP_Pengelola.Size = new Size(236, 27);
+            NP_Pengelola.TabIndex = 46;
+            // 
+            // PW_Pengelola
+            // 
+            PW_Pengelola.Location = new Point(479, 398);
+            PW_Pengelola.Name = "PW_Pengelola";
+            PW_Pengelola.Size = new Size(236, 27);
+            PW_Pengelola.TabIndex = 45;
+            // 
+            // US_Pengelola
+            // 
+            US_Pengelola.Location = new Point(479, 348);
+            US_Pengelola.Name = "US_Pengelola";
+            US_Pengelola.Size = new Size(236, 27);
+            US_Pengelola.TabIndex = 44;
+            // 
+            // JT_Pengelola
+            // 
+            JT_Pengelola.Location = new Point(479, 297);
+            JT_Pengelola.Name = "JT_Pengelola";
+            JT_Pengelola.Size = new Size(236, 27);
+            JT_Pengelola.TabIndex = 43;
+            // 
+            // JB_Pengelola
+            // 
+            JB_Pengelola.Location = new Point(479, 249);
+            JB_Pengelola.Name = "JB_Pengelola";
+            JB_Pengelola.Size = new Size(236, 27);
+            JB_Pengelola.TabIndex = 42;
+            // 
+            // AB_Pengelola
+            // 
+            AB_Pengelola.Location = new Point(479, 201);
+            AB_Pengelola.Name = "AB_Pengelola";
+            AB_Pengelola.Size = new Size(236, 27);
+            AB_Pengelola.TabIndex = 41;
+            // 
+            // NB_Pengelola
+            // 
+            NB_Pengelola.Location = new Point(479, 83);
+            NB_Pengelola.Name = "NB_Pengelola";
+            NB_Pengelola.Size = new Size(246, 27);
+            NB_Pengelola.TabIndex = 40;
+            // 
+            // cb_SepedaMotor
+            // 
+            cb_SepedaMotor.AutoSize = true;
+            cb_SepedaMotor.BackColor = Color.White;
+            cb_SepedaMotor.Location = new Point(479, 675);
+            cb_SepedaMotor.Name = "cb_SepedaMotor";
+            cb_SepedaMotor.Size = new Size(126, 24);
+            cb_SepedaMotor.TabIndex = 58;
+            cb_SepedaMotor.Text = "Sepeda Motor";
+            cb_SepedaMotor.UseVisualStyleBackColor = false;
+            // 
+            // cb_mobil
+            // 
+            cb_mobil.AutoSize = true;
+            cb_mobil.BackColor = Color.White;
+            cb_mobil.Location = new Point(479, 645);
+            cb_mobil.Name = "cb_mobil";
+            cb_mobil.Size = new Size(70, 24);
+            cb_mobil.TabIndex = 57;
+            cb_mobil.Text = "Mobil";
+            cb_mobil.UseVisualStyleBackColor = false;
+            // 
+            // cb_KendaraanBesar
+            // 
+            cb_KendaraanBesar.AutoSize = true;
+            cb_KendaraanBesar.BackColor = Color.White;
+            cb_KendaraanBesar.Location = new Point(555, 645);
+            cb_KendaraanBesar.Name = "cb_KendaraanBesar";
+            cb_KendaraanBesar.Size = new Size(237, 24);
+            cb_KendaraanBesar.TabIndex = 59;
+            cb_KendaraanBesar.Text = "Kendaraan besar (Truk, Bus, dll)";
+            cb_KendaraanBesar.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(-3, -2);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(806, 794);
+            pictureBox1.TabIndex = 29;
+            pictureBox1.TabStop = false;
+            // 
+            // bt_kembali
+            // 
+            bt_kembali.BackgroundImage = (Image)resources.GetObject("bt_kembali.BackgroundImage");
+            bt_kembali.FlatStyle = FlatStyle.Flat;
+            bt_kembali.ForeColor = SystemColors.Control;
+            bt_kembali.Location = new Point(520, 727);
+            bt_kembali.Name = "bt_kembali";
+            bt_kembali.Size = new Size(85, 29);
+            bt_kembali.TabIndex = 60;
+            bt_kembali.UseVisualStyleBackColor = true;
+            bt_kembali.Click += bt_kembali_Click;
+            // 
+            // RegisterPengelolaBengkel
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(label1);
-            Name = "Registrasi_Pengelola_Bengke_";
-            Text = "Registrasi_Pengelola_Bengke_";
+            ClientSize = new Size(799, 778);
+            Controls.Add(bt_kembali);
+            Controls.Add(cb_SepedaMotor);
+            Controls.Add(cb_mobil);
+            Controls.Add(cb_KendaraanBesar);
+            Controls.Add(L2_GantiOli);
+            Controls.Add(L1_ServisRutin);
+            Controls.Add(L5_ServisDarurat);
+            Controls.Add(PAdd);
+            Controls.Add(L3_ServisGaransi);
+            Controls.Add(AP_Pengelola);
+            Controls.Add(E_Pengelola);
+            Controls.Add(NT_Pengelola);
+            Controls.Add(NP_Pengelola);
+            Controls.Add(PW_Pengelola);
+            Controls.Add(US_Pengelola);
+            Controls.Add(JT_Pengelola);
+            Controls.Add(JB_Pengelola);
+            Controls.Add(AB_Pengelola);
+            Controls.Add(NB_Pengelola);
+            Controls.Add(pictureBox1);
+            Name = "RegisterPengelolaBengkel";
+            Text = "RegisterPengelolaBengkel";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private Label label1;
+        private CheckBox L2_GantiOli;
+        private CheckBox L1_ServisRutin;
+        private CheckBox L5_ServisDarurat;
+        private Button PAdd;
+        private CheckBox L3_ServisGaransi;
+        private TextBox AP_Pengelola;
+        private TextBox E_Pengelola;
+        private TextBox NT_Pengelola;
+        private TextBox NP_Pengelola;
+        private TextBox PW_Pengelola;
+        private TextBox US_Pengelola;
+        private TextBox JT_Pengelola;
+        private TextBox JB_Pengelola;
+        private TextBox AB_Pengelola;
+        private TextBox NB_Pengelola;
+        private CheckBox cb_SepedaMotor;
+        private CheckBox cb_mobil;
+        private CheckBox cb_KendaraanBesar;
+        private PictureBox pictureBox1;
+        private Button bt_kembali;
     }
 }
