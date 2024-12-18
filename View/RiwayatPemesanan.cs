cfa43f223afb0f6fc5fb49cb0bf510da753aba79
Nabila Laila <148866931+nabilalaila@users.noreply.github.com>
2024-12-18 22:09:46 +0700

    Update RiwayatPemesanan.cs

--- View/RiwayatPemesanan.cs
+++ View/RiwayatPemesanan.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using BengkelinAja___Final_Project.Context;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,19 +8,69 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static BengkelinAja___Final_Project.View.Login;
 
-namespace BengkelinAja.View
+namespace BengkelinAja___Final_Project.View
 {
-    public partial class RiwayatPemesanan : Form
+    public partial class Riwayat_Pemesanan : Form
     {
-        public RiwayatPemesanan()
+        public Riwayat_Pemesanan()
         {
             InitializeComponent();
+            LoadBengkelData();
+            this.StartPosition = FormStartPosition.CenterScreen;
         }
 
-        private void RiwayatPemesanan_Load(object sender, EventArgs e)
+        private void LoadBengkelData()
+        {
+            var pelangganContext = new PelangganContext();
+
+            try
+            {
+                string username = LoginSession.Username;
+                string password = LoginSession.Password;
+                pelangganContext.LoginPelangganGetId(username, password);
+                int id_pelanggan = LoginSession.PelangganId;
+                var dataTable = pelangganContext.RiwayatPemesanan(id_pelanggan);
+
+                if (dataTable != null && dataTable.Rows.Count > 0)
+                {
+                    dataGridView1.DataSource = dataTable;
+                    FormatGrid();
+                }
+                else
+                {
+                    MessageBox.Show($"Kamu belum pernah melakukan pesanan");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Terjadi kesalahan: " + ex.Message);
+            }
+        }
+
+
+        private void FormatGrid()
+        {
+            dataGridView1.Columns["waktu_transaksi"].HeaderText = "Waktu Transaksi";
+            dataGridView1.Columns["waktu_servis"].HeaderText = "Waktu Servis";
+            dataGridView1.Columns["nama_layanan"].HeaderText = "Jenis Layanan";
+            dataGridView1.Columns["waktu_jemput"].HeaderText = "Waktu penjemputan";
+            dataGridView1.Columns["alamat_penjemputan"].HeaderText = "Alamat Penjemputan";
+            dataGridView1.Columns["jenis_kendaraan"].HeaderText = "Jenis Kendaraan";
+            //dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            HomePagePelanggan homePagePelanggan = new HomePagePelanggan();
+            this.Close();
+            homePagePelanggan.Show();
         }
     }
 }
