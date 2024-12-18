686e963c3da57d7c3378bd1b08e64a0f5705d568
Nabila Laila <148866931+nabilalaila@users.noreply.github.com>
2024-12-18 22:10:07 +0700

    Update RiwayatPemesanan.Designer.cs

--- View/RiwayatPemesanan.Designer.cs
+++ View/RiwayatPemesanan.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace BengkelinAja.View
+namespace BengkelinAja___Final_Project.View
 {
-    partial class RiwayatPemesanan
+    partial class Riwayat_Pemesanan
     {
         /// <summary>
         /// Required designer variable.
@@ -28,19 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Riwayat_Pemesanan));
+            pictureBox1 = new PictureBox();
+            dataGridView1 = new DataGridView();
+            bt_Kembali = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
-            // RiwayatPemesanan
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(-1, -3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(801, 454);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.BackgroundColor = Color.White;
+            dataGridView1.BorderStyle = BorderStyle.None;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(52, 115);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(679, 229);
+            dataGridView1.TabIndex = 1;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // bt_Kembali
+            // 
+            bt_Kembali.BackColor = Color.Blue;
+            bt_Kembali.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            bt_Kembali.ForeColor = Color.White;
+            bt_Kembali.Location = new Point(644, 340);
+            bt_Kembali.Name = "bt_Kembali";
+            bt_Kembali.Size = new Size(105, 39);
+            bt_Kembali.TabIndex = 2;
+            bt_Kembali.Text = "Kembali";
+            bt_Kembali.UseVisualStyleBackColor = false;
+            bt_Kembali.Click += button1_Click;
+            // 
+            // Riwayat_Pemesanan
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Name = "RiwayatPemesanan";
-            Text = "RiwayatPemesanan";
-            Load += RiwayatPemesanan_Load;
+            Controls.Add(bt_Kembali);
+            Controls.Add(dataGridView1);
+            Controls.Add(pictureBox1);
+            Name = "Riwayat_Pemesanan";
+            Text = "Riwayat_Pemesanan";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private DataGridView dataGridView1;
+        private Button bt_Kembali;
     }
 }
