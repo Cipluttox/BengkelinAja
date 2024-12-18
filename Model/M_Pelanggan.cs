22a0c0bd6b7540c7d85439a0e002edbef41eaa41
Nabila Laila <148866931+nabilalaila@users.noreply.github.com>
2024-12-18 22:12:13 +0700

    Update M_Pelanggan.cs

--- Model/M_Pelanggan.cs
+++ Model/M_Pelanggan.cs
@@ -1,48 +1,82 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BengkelinAja.Model
+namespace BengkelinAja___Final_Project.Model
 {
-    public class M_Pelanggan
+    public abstract class M_Pelanggan
     {
-        public class dataAkun
+        public abstract class dataAkun
         {
             [Key]
             public int id_pelanggan { get; set; }
+
             [Required]
             public string nama_pelanggan { get; set; }
+
             [Required]
             public string username_pelanggan { get; set; }
+
             [Required]
-            public string password_pelanggan { get;set; }
+            public string password_pelanggan { get; set; }
+
             [Required]
             public string email_pelanggan { get; set; }
+
             [Required]
-            public string no_telp {  get; set; }
+            public string no_telp { get; set; }
+
             [Required]
             public string alamat_pelanggan { get; set; }
         }
-        public class Transaksi
+
+        public abstract class Transaksi
         {
             [Key]
             public int id_transaksi { get; set; }
+
             [Required]
             public DateTime waktu_transaksi { get; set; }
+
             [Required]
-            public TimeOnly waktu_servis { get; set; }
+            public DateTime waktu_servis { get; set; }
+
             [Required]
             public bool is_antar_jemput { get; set; }
-            [Required]
+
             public TimeOnly waktu_jemput { get; set; }
-            [Required]
+
             public string alamat_penjemputan { get; set; }
+
             [Required]
             public int id_kendaraan { get; set; }
-        } 
-        
+
+            [Required]
+            public int id_layanan { get; set; }
+        }
+
+        public abstract class detail_transaksi
+        {
+            [Required]
+            public int id_bengkel { get; set; }
+
+            [Required]
+            public int id_pelanggan { get; set; }
+
+            [Required]
+            public int id_transaksi { get; set; }
+        }
+        public class ConcreteDetailTransaksi : M_Pelanggan.detail_transaksi
+        {
+        }
+        public class ConcreteDataAkun : M_Pelanggan.dataAkun
+        {
+        }
+        public class ConcreteTransaksi : M_Pelanggan.Transaksi
+        {
+        }
     }
 }
